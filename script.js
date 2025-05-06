a2beb4e636b622df75e75476ca0934f10cafd911
zhou0478 <joanne.zhou@flinders.edu.au>
2025-05-06 10:10:33 +0930

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ const slides = [
         <li>feeling uncomfortable/insecure about your body when trying on clothes in a change-room, looking yourself in the mirror, getting ready with friends to go to a party, walking past a group of people who were looking at you, out in public</li>
       </ul>
     </div>
-    <p>Now take a moment to close your eyes and imagine this earliest event as if it is happening right now. Notice the thoughts and feelings that come up for you. Please only continue when you have a picture of this earliest event in your mind.</p>
+    <p>Now take a moment to close your eyes and imagine this earliest event as if it is happening <u>right now</u>. Notice the thoughts and feelings that come up for you. Please only continue when you have a picture of this earliest event in your mind.</p>
   </div>
   `,
   // Step 1 - Writing
   `
   <div class="slide">
     <p><strong>Step 1</strong></p>
-    <p>When you’re ready, in a few sentences, write about this earliest event you just thought about. Describe the event as if it is happening right now.</p>
+    <p>When you’re ready, in a few sentences, write about this <u>earliest</u> event you just thought about. Describe the event as if it is happening <u>right now</u>.</p>
     <div class="reminders">
       <p><strong>Try to:</strong></p>
       <ol>
@@ -39,7 +39,7 @@ const slides = [
         <li>Include as much detail as you can,such as what you are doing, who you are with, what you can see, how you are feeling, and what thoughts are going through your mind.</li>
       </ol>
     </div>
-    <textarea id="response1" placeholder="Write your reflection here..."></textarea>
+    <textarea id="response1" placeholder="Write about this memory following the instructions here..."></textarea>
   </div>
   `,
 
@@ -56,7 +56,7 @@ const slides = [
   `
   <div class="slide">
     <p><strong>Step 2</strong></p>
-    <p>Now, in a few sentences, write about this event as if it is happening right now...</p>
+    <p>Now, in a few sentences, write about this event as if it is happening right now, but this time, write about what someone else would see if they were watching the event happen from the outside, as if it were happening to your younger self right now. </p>
     <div class="reminders">
       <p><strong>Try to:</strong></p>
       <ol>
@@ -64,7 +64,7 @@ const slides = [
         <li>Include as much detail as you can, such as where Sarah is, what Sarah is doing, who Sarah is with, how Sarah might be feeling, and what thoughts might be going through Sarah’s mind.       </li>
       </ol>
     </div>
-    <textarea id="response2" placeholder="Write your reflection here..."></textarea>
+    <textarea id="response2" placeholder="Write about this memory following the instructions here..."></textarea>
   </div>
   `,
 
@@ -72,7 +72,7 @@ const slides = [
   `
   <div class="slide">
     <p><strong>Well done! Now let’s do Step 3, the final step…</strong></p>
-    <p>Think about the same memory one last time. This time, you are your younger self again (using “I” language), but your wiser and kinder older self is with you in the room. Your older self can get involved if you want them to. They can offer you kindness or provide new updated information based on what you know now, they can talk to you (or others) or do anything else that feels helpful and right in the situation.</p>
+    <p>Think about the same memory one last time. This time, <strong>you are your younger self again</strong> (using “I” language), but your wiser and kinder older self is with you in the room. Your older self can get involved if you want them to. They can offer you kindness or provide new updated information based on what you know now, they can talk to you (or others) or do anything else that feels helpful and right in the situation.</p>
     <p>Now take a moment to close your eyes and re-imagine this event as if it were happening to you right now. Remember, this time your wiser and kinder older self is with you and can get involved if you want them to. Please only continue when you have a picture of this in your mind.</p>
   </div>
   `,
@@ -81,7 +81,7 @@ const slides = [
   `
   <div class="slide">
     <p><strong>Step 3</strong></p>
-    <p>Now, in a few sentences, write about this event...</p>
+    <p>Now, in a few sentences, write about this event, as if it is happening right now, but this time, your wiser and older self is with you and can get involved if you want them to. </p>
     <div class="reminders">
       <p><strong>Try to:</strong></p>
       <ol>
@@ -90,7 +90,7 @@ const slides = [
         <li><strong>Describe what your wiser and kinder older self does in the situation.</strong> Remember, they can offer you kindness or provide new updated information based on what you know now, they can talk to you (or others) or do anything else that feels helpful and right in the situation.</li>
       </ol>
     </div>
-    <textarea id="response3" placeholder="Write your reflection here..."></textarea>
+    <textarea id="response3" placeholder="Write about this memory following the instructions here..."></textarea>
   </div>
   `,
 
